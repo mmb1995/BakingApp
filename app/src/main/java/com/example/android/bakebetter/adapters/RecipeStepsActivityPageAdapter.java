7abad78af4311141c699991e1f5a3c2179cbae87
mmb1995 <matthewblume95@gmail.com>
2019-01-28 13:06:41 -0800

    Refactored fragments to use ViewModels

--- app/src/main/java/com/example/android/bakebetter/adapters/RecipeStepsActivityPageAdapter.java
+++ app/src/main/java/com/example/android/bakebetter/adapters/RecipeStepsActivityPageAdapter.java
@@ -8,24 +8,18 @@ import android.support.v4.app.FragmentPagerAdapter;
 import com.example.android.bakebetter.R;
 import com.example.android.bakebetter.fragments.RecipeIngredientsFragment;
 import com.example.android.bakebetter.fragments.RecipeStepListFragment;
-import com.example.android.bakebetter.model.Ingredient;
-import com.example.android.bakebetter.model.Step;
-
-import java.util.ArrayList;
 
 public class RecipeStepsActivityPageAdapter extends FragmentPagerAdapter {
     private static final int NUM_ITEMS = 2;
     private static final String ID_KEY = "id";
     private final Context mContext;
-    private ArrayList<Step> mSteps;
-    private ArrayList<Ingredient> mIngredients;
+    private Long mRecipeId;
 
     public RecipeStepsActivityPageAdapter(Context context, FragmentManager fragmentManager,
-                                          ArrayList<Step> steps, ArrayList<Ingredient> ingredients) {
+                                          Long recipeId) {
         super(fragmentManager);
         this.mContext = context;
-        this.mSteps = steps;
-        this.mIngredients = ingredients;
+        this.mRecipeId = recipeId;
     }
 
     @Override
@@ -33,13 +27,13 @@ public class RecipeStepsActivityPageAdapter extends FragmentPagerAdapter {
         Fragment fragment;
         switch (position) {
             case 0:
-                fragment = RecipeIngredientsFragment.newInstance(mIngredients);
+                fragment = RecipeIngredientsFragment.newInstance(mRecipeId);
                 break;
             case 1:
-                fragment = RecipeStepListFragment.newInstance(mSteps);
+                fragment = RecipeStepListFragment.newInstance(mRecipeId);
                 break;
             default:
-                fragment = RecipeStepListFragment.newInstance(mSteps);
+                fragment = RecipeStepListFragment.newInstance(mRecipeId);
         }
         return fragment;
     }
--- app/src/main/java/com/example/android/bakebetter/di/modules/ViewModelModule.java
+++ app/src/main/java/com/example/android/bakebetter/di/modules/ViewModelModule.java
@@ -5,7 +5,9 @@ import android.arch.lifecycle.ViewModelProvider;
 
 import com.example.android.bakebetter.di.ViewModelKey;
 import com.example.android.bakebetter.viewmodels.FactoryViewModel;
+import com.example.android.bakebetter.viewmodels.IngredientsViewModel;
 import com.example.android.bakebetter.viewmodels.RecipeListViewModel;
+import com.example.android.bakebetter.viewmodels.StepsViewModel;
 
 import dagger.Binds;
 import dagger.Module;
@@ -18,6 +20,18 @@ public abstract class ViewModelModule {
     @IntoMap
     @ViewModelKey(RecipeListViewModel.class)
     abstract ViewModel bindRecipeListViewModel(RecipeListViewModel recipeListViewModel);
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(IngredientsViewModel.class)
+    abstract ViewModel bindIngredientsViewModel(IngredientsViewModel ingredientsViewModel);
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(StepsViewModel.class)
+    abstract ViewModel bindStepsListViewModel(StepsViewModel stepsViewModel);
+
+
 
     @Binds
     abstract ViewModelProvider.Factory bindViewModelFactory(FactoryViewModel factory);
